7637fe84e3d76867a3d9bbc7169e19d28bf0dab9
Shapar <nkerah_w@mail.ru>
2010-10-27 18:13:02 +0300

    basic

--- includes/dthread.cpp
+++ includes/dthread.cpp
@@ -1,13 +1,27 @@
+/*
+    This file is part of OpenDAQ.   
+    Copyright (C) Shapar Andrei (email: nkerah_w@mail.ru)
+
+    OpenDAQ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "dthread.h"
 
 DThread::DThread()
 { //конструктор
 	device = new UHandler();	// создаём переменную управления устройством
-//	timer = new QTimer(this);
 	config = new DConfig;
-//	connect (timer, SIGNAL (timeout()), device, SLOT (conversion_request()) ); // по переполнению таймера - вызов преобразования
-	/* лучше брать сигнал о завершении из uhandler */
-//	connect (timer, SIGNAL (timeout()), this, SLOT (update())); // обновление счётчика периодов
 	connect (device, SIGNAL (data_ready(DPack *)), this, SLOT (deliver_data (DPack *)) ); // связываем устройство с потоком
 
 	stopped = false;
--- includes/dthread.h
+++ includes/dthread.h
@@ -1,3 +1,21 @@
+/*
+    This file is part of OpenDAQ.   
+    Copyright (C) Shapar Andrei (email: nkerah_w@mail.ru)
+
+    OpenDAQ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef DTHREAD_H
 #define DTHREAD_H
 #include <unistd.h> // usleep ();
@@ -18,7 +36,6 @@ public:
 	DThread(); 
 	DConfig *config;			// настройки
 	UHandler *device;
-//	QTimer *timer;
 
 public slots:
 	//void deliver_data ();	// приём резльутата от device
@@ -26,7 +43,6 @@ public slots:
 	void set_config (DConfig *cfg);	// установка конфигурации 	
 	void stop_thread();		// остановка выполнения потока
 	void start_thread();
-//	void update();		// обновление состояния счётчика, завершение работы потока, если необходимо
 
 protected:
 	void run();	// перегружаем стандартную функцию работы потока, вносим туда весь полезный код.
